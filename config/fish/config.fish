98495d4b7e20559cd4948e51f656be5c2d44a897
Patrick Insinger <pat@delta.local>
2017-06-11 02:36:57 -0400

    Upgrades for latest fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,7 +2,14 @@ if status --is-interactive
     eval sh $HOME/.config/base16-shell/scripts/base16-google-dark.sh
 end
 
+function setenv
+    set -gx $argv
+end
+
 status --is-interactive; and source (pyenv init -|psub)
+
+set fish_greeting ""
+fish_vi_key_bindings
 
 export FZF_DEFAULT_COMMAND='ag -g ""'
 
